4c4d335f20591ad45989d7adb0523ae12584e642
k5321116 <k5321116@icloud.com>
2026-02-21 11:50:31 +0900

    認証機能を整理し、スコープを引数として受け取るように変更

--- get_token.py
+++ get_token.py
@@ -1,18 +1,28 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
-import os
 import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.auth.exceptions import GoogleAuthError
 
-# スコープの定義（Gmailの全機能を使うためのスコープ）
-SCOPES = ['https://mail.google.com/']
+def authenticate(scope):
+    creds = None
+    if not creds or not creds.valid:
+        try:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', scope)
+                
+                creds = flow.run_local_server(port=0)
+        except GoogleAuthError as err:
+            print(f'action=authenticate error={err}')
+            raise
 
-def main():
-    # credentials.jsonはGCPからダウンロードしたファイル
-    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-    creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-    # 取得したトークンを保存しておく（token.pickleに保存）
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
+    return creds
 
 if __name__ == '__main__':
-    main()
+    authenticate(['https://mail.google.com/'])
